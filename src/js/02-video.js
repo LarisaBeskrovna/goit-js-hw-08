6e4c6607ad04c1593db905a0fd46457995c0575d
LarisaBeskrovna <117515454+LarisaBeskrovna@users.noreply.github.com>
2023-05-02 15:23:04 +0300

    error

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -9,26 +9,19 @@ player.on('timeupdate', function () {
     console.log('played the video!');
     const currentTime = player.currentTime();
     localStorage.setItem(KEY_PLAUER, throttle(currentTime,1000));
-    {
-        duration: 61.857
-        percent: 0.049
-        seconds: 3.034
-    }
-    
-  }); 
+    throttle
+        {
+            duration: 61.857
+            percent: 0.049
+            seconds: 3.034
+        }}); 
+        console.log(KEY_PLAUER);
+        const restoreCurrentTime = () => {
+        const currentTime = localStorage.getItem(KEY_PLAUER);
+          if (currentTime) {
+            player.setCurrentTime(currentTime)
+          }
+        }
+        restoreCurrentTime()
 
-function playerPaused(){
-    if(JSON.parse(localStorage.setItem(KEY_PLAUER))===null){
-        return;
-    }}
- const pausedPlayer = JSON.parse(localStorage.setItem(KEY_PLAUER))["second"];
-
- if (pausedPlayer) {
-    player
-    .setCurrentTime(pausedPlayer)
-    .then(function(second){})
-    .catch(function(error){switch (error.name) {
-        case 'Error':break;}})
-    
- }
 
